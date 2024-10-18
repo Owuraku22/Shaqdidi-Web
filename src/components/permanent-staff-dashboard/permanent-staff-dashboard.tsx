bd71a4fa1fd9e490fc39da3e1bb3bda6acefb7bf
gustonstomy <gustomstomy@gmail.com>
2024-10-18 16:37:41 +0100

    changes in font, added popover and drawer to order page, created details page

--- src/components/permanent-staff-dashboard/permanent-staff-dashboard.tsx
+++ src/components/permanent-staff-dashboard/permanent-staff-dashboard.tsx
@@ -40,41 +40,57 @@ const PsDashboardPage = () => {
         </h2>
       </div>
       <div className="relative w-64 my-4 lg:m-4 md:mb-8 lg:ml-4">
-        <Input
-          type="text"
-          placeholder="Search"
-          // value={search}
-          // onChange={handleSearch}
-          className="pl-10"
-        />
-        <Icons.Search
-          className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 h-4 w-4"
-          // size={20}
-        />
+        <Input type="text" placeholder="Search" className="pl-10" />
+        <Icons.Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 h-4 w-4" />
       </div>
 
       <div className="grid gap-y-4 gap-x-2 lg:gap-5 md:gap-y-0 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 3xl:grid-5 overflow-auto  md:ml-0 2xl:mx-5 w-[22.4em] md:w-[47em] lg:w-[80em] 2xl:w-[80em] 3xl:w-[95em]">
         {data.map((values, index) => (
-          <PopoverForm value={values} key={index}>
-            <div className="md:my-4 rounded-xl  cursor-pointer border h-[9.5em] md:md:h-[12em]  lg:h-[13.5em] w-[10.7em] md:w-[14.5em] first-letter: lg:w-[18em] 2xl:w-[18.7em] ">
-              <div className="h-[6em] md:h-[7.5em] lg:h-[9em] rounded-t-xl object-cover mb-1 border ">
-                <img
-                  src="/hamburger.png"
-                  className="h-[6em] md:h-[7.5em]  lg:h-[9em] w-full rounded-t-xl object-cover object-center"
-                />
-              </div>
-              <h1
-                key={index}
-                className="flex px-2 md:px-4 pt-1 font-bold text-sm md:text-[1.1em] "
-              >
-                {/* {values.name} */}
-                Daavi’s Special Gob3
-              </h1>
-              <h1 className="flex px-2 md:px-4 text-[0.8em] md:text-sm font-roboto">
-                {values.address}
-              </h1>
-            </div>
-          </PopoverForm>
+          <>
+            {isMobile ? (
+              <DrawerForm value={values} key={index}>
+                <div className="md:my-4 rounded-xl  cursor-pointer border h-[9.5em] md:md:h-[12em]  lg:h-[13.5em] w-[10.7em] md:w-[14.5em] first-letter: lg:w-[18em] 2xl:w-[18.7em] ">
+                  <div className="h-[6em] md:h-[7.5em] lg:h-[9em] rounded-t-xl object-cover mb-1 border ">
+                    <img
+                      src="/hamburger.png"
+                      className="h-[6em] md:h-[7.5em]  lg:h-[9em] w-full rounded-t-xl object-cover object-center"
+                    />
+                  </div>
+                  <h1
+                    key={index}
+                    className="flex px-2 md:px-4 pt-1 font-bold text-sm md:text-[1.1em] "
+                  >
+                    {/* {values.name} */}
+                    Daavi’s Special Gob3
+                  </h1>
+                  <h1 className="flex px-2 md:px-4 text-[0.8em] md:text-sm font-roboto">
+                    {values.address}
+                  </h1>
+                </div>
+              </DrawerForm>
+            ) : (
+              <PopoverForm value={values} key={index}>
+                <div className="md:my-4 rounded-xl  cursor-pointer border h-[9.5em] md:md:h-[12em]  lg:h-[13.5em] w-[10.7em] md:w-[14.5em] first-letter: lg:w-[18em] 2xl:w-[18.7em] ">
+                  <div className="h-[6em] md:h-[7.5em] lg:h-[9em] rounded-t-xl object-cover mb-1 border ">
+                    <img
+                      src="/hamburger.png"
+                      className="h-[6em] md:h-[7.5em]  lg:h-[9em] w-full rounded-t-xl object-cover object-center"
+                    />
+                  </div>
+                  <h1
+                    key={index}
+                    className="flex px-2 md:px-4 pt-1 font-bold text-sm md:text-[1.1em] "
+                  >
+                    {/* {values.name} */}
+                    Daavi’s Special Gob3
+                  </h1>
+                  <h1 className="flex px-2 md:px-4 text-[0.8em] md:text-sm font-roboto">
+                    {values.address}
+                  </h1>
+                </div>
+              </PopoverForm>
+            )}
+          </>
         ))}
       </div>
     </div>
