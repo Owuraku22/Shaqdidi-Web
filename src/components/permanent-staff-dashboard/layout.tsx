01342e886e10a1a318a3ec8b8295336b30c77300
Meshack Commey <commeymeshack@gmail.com>
2024-10-18 09:51:50 +0000

    added side-bar and order-history component to layout

--- src/components/permanent-staff-dashboard/layout.tsx
+++ src/components/permanent-staff-dashboard/layout.tsx
@@ -1,24 +1,31 @@
 import { Outlet } from "react-router-dom";
 import Menu from "@/components/menu";
 import Navbar from "@/components/navbar";
+import { ScrollArea } from "../ui/scroll-area";
 
 
 export default function Layout() {
     return(
     <section className="fixed flex w-full h-screen">
         {/* SIDE BAR*/}
-        <div className="hidden lg:block flex-1 px-4">
-            <div className="py-10"></div>
-            <Menu />
-        </div>
-        <div className="lg:hidden fixed z-50 left-0 top-0"></div>
-        <div className="flex-[8] w-full bg-white">
+        <aside className="hidden lg:block flex-1 px-4">
+            <div className="p-4 flex justify-between md:justify-center items-center w-full">
+                <h2 className="text-xl font-bold md:hidden block text-left">ShaQ D|D|</h2>
+                <div className="lg:flex items-center space-x-4 md:block hidden">
+                    <img src="/logo.svg" alt="ShaQ D|D" className="h-24" />
+                </div>
+            </div>
+            <Menu /> {/* SIDE MENU */}
+        </aside>
+        <div className="lg:flex-[5] xl:flex-[6] 2xl:flex-[7] w-full bg-white">
             {/** NAV BAR */}
             <Navbar />
 
             {/** MAIN CONTENT */}
-            <main className="w-full h-screen p-4 text-slate-900">
-                <Outlet />
+            <main className="w-full h-screen p-4 lg:px-8 text-slate-900">
+                <ScrollArea className="w-auto h-screen whitespace-nowrap pb-24">
+                    <Outlet />
+                </ScrollArea>
             </main>
         </div>
     </section>
